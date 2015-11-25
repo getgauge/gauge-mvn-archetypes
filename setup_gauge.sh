d54a0ce71228d39340a12801f84ba9fe9c7cf0c2
apoorvam <apoorva_rarefriend@yahoo.com>
2015-11-25 14:42:04 +0530

    adding seup_gauge.sh to install gauge and gauge-java

---
+++ setup_gauge.sh
@@ -0,0 +1,11 @@
+rm -rf $GOPATH/src $GOPATH/pkg
+
+go get -d -u -v github.com/getgauge/gauge && go get github.com/tools/godep
+
+cd $GOPATH/src/github.com/getgauge/gauge && $GOBIN/godep restore
+
+cd $GOPATH/src/github.com/getgauge/gauge && go run build/make.go && go run build/make.go --install --prefix /tmp/
+
+go get -d -u -v github.com/getgauge/gauge-java
+
+cd $GOPATH/src/github.com/getgauge/gauge-java && go run make.go && go run make.go --install
